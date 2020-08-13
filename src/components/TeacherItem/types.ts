f500fa0a513e6cba0161cd04d4447346c81e0464
Daniel Zordan <daniel.zordan@vtinova.com.br>
2020-08-13 15:08:29 -0400

    mobile finished

---
+++ src/components/TeacherItem/types.ts
@@ -0,0 +1,15 @@
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  name: string;
+  subject: string;
+  user_id: number;
+  whatsapp: string;
+}
+
+export interface TeacherItemProps {
+  teacher: Teacher;
+  favorited: boolean;
+}
---
+++ src/pages/TeacherList/types.ts
@@ -0,0 +1,10 @@
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  name: string;
+  subject: string;
+  user_id: number;
+  whatsapp: string;
+}
